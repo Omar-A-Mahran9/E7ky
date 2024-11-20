ec003326f0f5e4faa49ff2dcccaf6d029cda5b90
Omar Abdullah Mahran <109427483+Omar-A-Mahran9@users.noreply.github.com>
2024-11-20 18:55:19 +0200

    edit some

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // AdminSeeder::class,
+            AdminSeeder::class,
             RoleSeeder::class,
             // BrandSeeder::class,
             // CategorySeeder::class,
-            // VendorSeeder::class
+            VendorSeeder::class
         ]);
     }
 }
