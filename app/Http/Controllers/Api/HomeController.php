5e803b919796b6be3f5739ae3b052d6baa86967b
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-05-22 22:26:48 +0300

    firat commit

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\ArticleResource;
 use App\Http\Resources\Api\EventResource;
+use App\Models\Article;
 use App\Models\Event;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -11,52 +13,34 @@ use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
-        $banners = Event::where('featured', 1)
+
+public function index()
+{
+    // Get featured events starting today or later
+    $banners = Event::where('featured', 1)
         ->whereDate('start_day', '>=', Carbon::today())
         ->get();
-        $response = Http::get('https://e7kky.com/api/v1/app/articles');
-        $data = $response->json();
 
-        if ($data['status'] && isset($data['data']['articles']['data'])) {
-            $articles = $data['data']['articles']['data'];
-        } else {
-            $articles = [];
-        }
+    // Get latest 5 articles
+    $articles = Article::latest()->take(10)->get();
 
-        $articles = collect($articles)->map(function ($article) {
-            return [
-                'id' => $article['id'],
-                'title' => $article['name'],
-                'description' => $article['description'],
-                'name' => $article['name'],
-                'image' => "https://e7kky.com/" . $article['image']
-            ];
-        })->take(5); // Limit to 5 articles
+    $cycle_tracking = [
+        'title' => 'Cycle Tracking',
+        'description' => 'Track your menstrual cycle and get insights into your fertility and overall health.',
+        'image' => '',
+    ];
 
-        $cycle_tracking = [
-            'title' => 'Cycle Tracking',
-            'description' => 'Track your menstrual cycle and get insights into your fertility and overall health.',
-            'image' => ''
-        ];
+    $invite_friend = [
+        'title' => 'Invite a Friend',
+        'description' => 'Invite your friends to join the app and enjoy exclusive features together.',
+        'image' => '',
+    ];
 
-        $invite_friend = [
-            'title' => 'Cycle Tracking',
-            'description' => 'Track your menstrual cycle and get insights into your fertility and overall health.',
-            'image' => ''
-        ];
-
-        $fully_data = [
-            'banners' => EventResource::collection($banners) ,
-            'cycle_tracking' => $cycle_tracking,
-            'articles' => $articles,
-            'invite_friend' => $invite_friend
-        ];
- return $this->success('test',data: [
-    'banners' => EventResource::collection($banners),
-     'articles' => $articles,
- ]);
-
-    }
+    return $this->success('Data fetched successfully', data: [
+        'banners' => EventResource::collection($banners),
+        'cycle_tracking' => $cycle_tracking,
+        'articles' => ArticleResource::collection($articles),
+        'invite_friend' => $invite_friend,
+    ]);
+}
 }
